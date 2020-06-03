e43d2a8ec9cae8427ed09e04047687369fd4233b
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2020-06-02 21:21:11 -0400

    Agregar archivos de proyecto.

---
+++ Model/MiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerVistazoASPNETcore.Model
+{
+    public class MiDbContext : DbContext
+    {
+        public DbSet<Producto> Producto { get; set; }
+        public MiDbContext(DbContextOptions<MiDbContext> opciones) : base(opciones)
+        {
+
+        }
+    }
+}
---
+++ Model/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerVistazoASPNETcore.Model
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public float Precio { get; set; }
+    }
+}
