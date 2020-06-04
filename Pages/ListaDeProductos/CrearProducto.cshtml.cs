89e5d796398504cb009a4f2ce3a7d059013c92b6
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2020-06-03 21:16:43 -0400

    Sesión 2: Rescatando registros desde DDBB y Creando registros nuevos con validación.

---
+++ Pages/ListaDeProductos/CrearProducto.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PrimerVistazoASPNETcore.Model;
+
+namespace PrimerVistazoASPNETcore.Pages.ListaDeProductos
+{
+    public class CrearProductoModel : PageModel
+    {
+        private readonly MiDbContext DB;
+        [BindProperty]
+        public Producto Producto { get; set; }
+        public CrearProductoModel(MiDbContext db) {
+            DB = db;
+        }
+        public void OnGet()
+        {
+
+        }
+        public async Task<IActionResult> OnPost() {
+            if (ModelState.IsValid)
+            {
+                await DB.Producto.AddAsync(Producto);
+                await DB.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else {
+                return Page();
+            }
+        }
+    }
+}
---
+++ Pages/ListaDeProductos/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PrimerVistazoASPNETcore.Model;
+
+namespace PrimerVistazoASPNETcore.Pages.ListaDeProductos
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MiDbContext DB;
+        public IEnumerable<Producto> Productos { get; set; }
+        public IndexModel(MiDbContext db) {
+            DB = db;
+        }
+        public async Task OnGet()
+        {
+            Productos = await DB.Producto.ToListAsync();
+        }
+    }
+}
